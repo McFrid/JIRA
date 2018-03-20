d01fcd223d216c0e2285d6ec7e6ca2f948e7e616
RomanLetsuk <romaletsuk@gmail.com>
2018-03-20 23:13:38 +0300

    added reducers for users actions

--- frontend/src/reducers/users.js
+++ frontend/src/reducers/users.js
@@ -1,46 +1,70 @@
 import actionTypes from '../actions/actionTypes';
 
-const initState = [{
-  id: 1,
-  email: 'test@google.com',
-  firstName: 'Firts Name 1',
-  lastName: 'Last Name 1',
-  experience: 1.2,
-}, {
-  id: 2,
-  email: 'test2@google.com',
-  firstName: 'Firts Name 2',
-  lastName: 'Last Name 2',
-  experience: 2.2,
-}];
+const initState = {
+  items: [{
+    id: 1,
+    email: 'test@google.com',
+    firstName: 'Firts Name 1',
+    lastName: 'Last Name 1',
+    experience: 1.2,
+  }, {
+    id: 2,
+    email: 'test2@google.com',
+    firstName: 'Firts Name 2',
+    lastName: 'Last Name 2',
+    experience: 2.2,
+  }],
+  isFetching: false,
+};
 
 const userState = (state = initState, action) => {
   switch (action.type) {
     case actionTypes.user.USER_STORE:
-      return [
+      return {
         ...state,
-        {
-          ...action.payload.user,
-          id: Math.random(),
-          experience: Number.parseFloat(action.payload.user.experience),
-        },
-      ];
+        items: [
+          ...state.items,
+          {
+            ...action.payload.user,
+            id: Math.random(),
+            experience: Number.parseFloat(action.payload.user.experience),
+          },
+        ],
+      };
 
     case actionTypes.user.USER_UPDATE:
-      return state.map((user) => {
-        if (user.id === action.payload.id) {
-          return {
-            ...action.payload.user,
-            id: action.payload.id,
-            experience: Number.parseFloat(action.payload.user.experience),
-          };
-        }
+      return {
+        ...state,
+        items: state.items.map((user) => {
+          if (user.id === action.payload.id) {
+            return {
+              ...action.payload.user,
+              id: action.payload.id,
+              experience: Number.parseFloat(action.payload.user.experience),
+            };
+          }
 
-        return user;
-      });
+          return user;
+        }),
+      };
 
     case actionTypes.user.USER_REMOVE:
-      return state.filter(user => user.id !== action.payload.id);
+      return {
+        ...state,
+        items: state.items.filter(user => user.id !== action.payload.id),
+      };
+
+    case actionTypes.users.USERS_FETCH:
+      return {
+        ...state,
+        isFetching: true,
+      };
+
+    case actionTypes.users.USERS_STORE:
+      return {
+        ...state,
+        isFetching: false,
+      };
 
     default:
       return state;
