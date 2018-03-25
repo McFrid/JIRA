3056a64252416a654b15252c65a7e59cee7ebd47
Alexander Yakusik <alexander.yakusik@gmail.com>
2018-03-25 17:52:17 +0300

    Added actions for fetching and storing roles.

---
+++ frontend/src/actions/roles/roles.js
@@ -0,0 +1,23 @@
+import actionTypes from "../actionTypes";
+import rolesService from '../../services/roles-service'
+
+const storeRoles = roles => ({
+  type: actionTypes.roles.ROLES_STORE,
+  payload: {
+    roles,
+  }
+});
+
+const fetchRoles = async (dispatch) => {
+  dispatch({
+    type: actionTypes.roles.ROLES_FETCH
+  });
+
+  const roles = rolesService.fetchRoles();
+
+  dispatch(storeRoles(roles));
+};
+
+export default {
+  fetchRoles,
+}
