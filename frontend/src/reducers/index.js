de3cc2f16181a027d20058265783c21c9b3f6cc8
Unknown <romaletsuk@gmail.com>
2018-05-04 02:06:03 +0300

    added reducers for account details

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import users from './users';
 import roles from './roles';
+import account from './account';
 
 export default combineReducers({
   users,
   roles,
+  account,
 });
