4e35b8120181a0c5bbd8bc10b14fe17cc76c96ed
RomanLetsuk <romaletsuk@gmail.com>
2018-05-03 19:19:16 +0300

    updated request

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -1,6 +1,8 @@
+import axios from 'axios';
+
 import auth from './auth';
 
-const baseUrl = 'http://localhost:8080/api/';
+const baseUrl = 'http://localhost:8080/api';
 
 const generateHeaders = (customHeaders) => {
   let headers = {
@@ -18,8 +20,38 @@ const generateHeaders = (customHeaders) => {
   return headers;
 };
 
-const getUrl
-
 const makeRequest = (url, dataOrParams, method, customHeaders) => {
-  const url =
+  const otp = {
+    baseURL: baseUrl,
+    url,
+    method,
+    dataType: 'json',
+    headers: generateHeaders(customHeaders),
+    timeout: 120000,
+    data: dataOrParams,
+  };
+
+  if (method === 'GET' || method === 'DELETE') {
+    otp.params = dataOrParams;
+  }
+
+  return axios(otp);
+};
+
+export default {
+  get(url, params, headers) {
+    return makeRequest(url, params, 'GET', headers);
+  },
+
+  del(url, params, headers) {
+    return makeRequest(url, params, 'DELETE', headers);
+  },
+
+  post(url, data, headers) {
+    return makeRequest(url, data, 'POST', headers);
+  },
+
+  put(url, data, headers) {
+    return makeRequest(url, data, 'PUT', headers);
+  },
 };
