8fe97290c45901244bed8c4b07d085d074219401
Denis Urban <freezex69@gmail.com>
2018-05-06 12:12:36 +0300

    REST-controller added.

--- backend/src/main/java/by/bsuir/mpp/xpulse/service/DocumentService.java
+++ backend/src/main/java/by/bsuir/mpp/xpulse/service/DocumentService.java
@@ -1,65 +1,8 @@
 package by.bsuir.mpp.xpulse.service;
 
-import by.bsuir.mpp.xpulse.config.Constants;
-import net.sf.dynamicreports.jasper.builder.JasperReportBuilder;
-import net.sf.dynamicreports.jasper.builder.export.JasperCsvExporterBuilder;
-import net.sf.dynamicreports.jasper.builder.export.JasperPdfExporterBuilder;
-import net.sf.dynamicreports.jasper.builder.export.JasperXlsExporterBuilder;
-import net.sf.jasperreports.engine.JasperExportManager;
-import net.sf.jasperreports.engine.JasperPrint;
-import net.sf.jasperreports.engine.export.ooxml.JRXlsxExporter;
-import net.sf.jasperreports.export.SimpleExporterInput;
-import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
-import org.springframework.core.io.InputStreamResource;
+
 import org.springframework.stereotype.Service;
-
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.OutputStream;
-
-import static net.sf.dynamicreports.report.builder.DynamicReports.export;
-
 
 @Service
 public class DocumentService {
-
-    private ByteArrayOutputStream baos;
-
-    public byte[] writeTo(JasperReportBuilder jp, final String format) throws Exception {
-        if (format.equalsIgnoreCase(Constants.PDF)) {
-            writeToPdf(jp);
-        }
-        else if (format.equalsIgnoreCase(Constants.XLS)) {
-            writeToXls(jp);
-        }
-        else {
-            writeToCsv(jp);
-        }
-        return baos.toByteArray();
-    }
-
-    private void writeToCsv(JasperReportBuilder reportBuilder) throws Exception {
-        JasperCsvExporterBuilder csvExporterBuilder = export
-            .csvExporter(baos)
-            .setCharacterEncoding("UTF-8");
-
-        reportBuilder.toCsv(csvExporterBuilder);
-    }
-
-    private void writeToXls(JasperReportBuilder reportBuilder) throws Exception {
-        JasperXlsExporterBuilder xlsExporterBuilder = export
-            .xlsExporter(baos)
-            .setCharacterEncoding("UTF-8");
-
-        reportBuilder.toXls(xlsExporterBuilder);
-    }
-
-    private void writeToPdf(JasperReportBuilder reportBuilder) throws Exception {
-        JasperPdfExporterBuilder pdfExporterBuilder = export
-            .pdfExporter(baos)
-            .setCharacterEncoding("UTF-8");
-
-        reportBuilder.toPdf(pdfExporterBuilder);
-    }
-
 }
--- backend/src/main/java/by/bsuir/mpp/xpulse/web/rest/DocumentResource.java
+++ backend/src/main/java/by/bsuir/mpp/xpulse/web/rest/DocumentResource.java
@@ -6,9 +6,7 @@ import by.bsuir.mpp.xpulse.service.DocumentService;
 import com.codahale.metrics.annotation.Timed;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
-import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,57 +19,13 @@ public class DocumentResource {
 
     private final DocumentService documentService;
 
-    @Autowired
     public DocumentResource(DocumentService documentService) {
         this.documentService = documentService;
     }
 
-    /**
-     * Contributions of specified user (Which issues user is solving)
-     * @param login
-     * @param format
-     * @return
-     */
     @GetMapping("/users/{login:" + Constants.LOGIN_REGEX + "}/contributions")
     @Timed
-    public ResponseEntity<Resource> downloadContributions
-        (@PathVariable String login, @RequestParam String format) {
-
-        return null;
-
-    }
-
-    /**
-     * Solution statistics (Resolved date, estimate, etc.)
-     * @param format
-     * @return
-     */
-    @GetMapping("/solutions/statistic")
-    @Timed
-    public ResponseEntity<Resource> downloadSolutionStatistics(@RequestParam String format) {
-        return null;
-    }
-
-    /**
-     * Product statistics (issues number, solved, manager, etc.)
-     * @param format
-     * @return
-     */
-    @GetMapping("/products/statistic")
-    @Timed
-    public ResponseEntity<Resource> downloadProductStatistics(@RequestParam String format) {
-        return null;
-    }
-
-    /**
-     *  Resolved issues by specified user
-     * @param login
-     * @param format
-     * @return
-     */
-    @GetMapping("/users/{login:" + Constants.LOGIN_REGEX + "}/resolved")
-    @Timed
-    public ResponseEntity<Resource> downloadResolvedIssues
+    public ResponseEntity<InputStreamResource> downloadContributions
         (@PathVariable String login, @RequestParam String format) {
 
         return null;
